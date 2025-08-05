975870065f4f2ae2790282f66cf135cfccdeb248
shubhivermax <shubhiverma@usf.edu>
2025-08-05 12:55:52 -0400

    images bg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import logo from '../assets/logo.png';
+import fashion1 from '../assets/fashion1.png';
+import fashion2 from '../assets/fashion2.png';
 
 const Home = () => {
   return (
-    <div className='homepage'>Homepage
-    <div className='homepage-content'>
-
+    <div className='homepage'>
+      <div className='fashion-image-container'>
+        <img src={fashion1} alt="fashion1" className='fashion-image fashion-image-left' />
+        <img src={fashion2} alt="fashion2" className='fashion-image fashion-image-right' />
+      </div>
+      <div className='homepage-content'>
         <div className='homepage-logo'>
-            <img src={logo} alt="logo" className='homepage-logo-img' />
+          <img src={logo} alt="logo" className='homepage-logo-img' />
         </div>
-    </div>
-    
-    
+      </div>
     </div>
   )
 }
