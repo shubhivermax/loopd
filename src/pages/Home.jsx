29c96598a130a15d738d9b7210cc40a6c25739b0
shubhivermax <shubhiverma@usf.edu>
2025-08-05 12:31:23 -0400

    logos added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import logo from '../assets/logo.png';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homepage'>Homepage
+    <div className='homepage-content'>
+
+        <div className='homepage-logo'>
+            <img src={logo} alt="logo" className='homepage-logo-img' />
+        </div>
+    </div>
+    
+    
+    </div>
   )
 }
 
