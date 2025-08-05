61a598f63c8e788ffed222b1a03a70521894a019
shubhivermax <shubhiverma@usf.edu>
2025-08-05 12:17:04 -0400

    navbar w links added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import '../App.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <div className='logo'>
+        <img src={logo} alt="logo" className="logo-img"/>
+      </div>
       <div className="nav-links">
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/About" className="nav-link">About</Link>
